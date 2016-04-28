d9553fa0d0db0df7fa3e7c7af8798eee79fee519
Hidenori FUJIMURA <hfu@si.local>
2016-04-29 00:55:20 +0900

    update

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 [wip] GeoJSON API for JIS X 0410
 
 ## Usage
-to get GeoJSON for the grid code 5340, access http://madefor.github.io/0410/api/v1/5340.geojson . In addition, https://github.com/madefor/0410/blob/gh-pages/api/v1/5340.geojson will show the grid on the map by GitHub. 
+- to get GeoJSON for the grid code 5340, access http://madefor.github.io/0410/api/v1/5340.geojson .
+- In addition, https://github.com/madefor/0410/blob/gh-pages/api/v1/5340.geojson will show the grid on the map by GitHub. 
 
 ## Reference 
 - JIS X 0410 地域メッシュコード http://kikakurui.com/x0/X0410-2002-01.html
--- build.rb
+++ build.rb
@@ -1,28 +1,31 @@
 require 'json'
+def dump(code, lng0, lat0, lng1, lat1)
+  File.open("api/v1/#{code}.geojson", 'w') {|w|
+    w.print JSON.dump({
+      :type => 'Feature',
+      :geometry => {
+        :type => 'Polygon',
+        :coordinates => [[
+          [lng0, lat0], [lng0, lat1],
+          [lng1, lat1], [lng1, lat0], [lng0, lat0]
+        ]]
+      },
+      :properties => {
+        :code => code,
+        :url => "http://madefor.github.io/0410/api/v1/#{code}.geojson",
+        :view => "https://github.com/madefor/0410/blob/gh-pages/api/v1/#{code}.geojson"
+      }
+    })
+  }
+end
+
 30.upto(68) {|n1|
   22.upto(53) {|n2|
-    code = "#{n1}#{n2}".to_i
-    lng0 = n2 + 100.0
-    lat0 = n1 * 2.0 / 3
-    lng1 = (n2 + 1) + 100.0
-    lat1 = (n1 + 1) * 2.0 / 3
-    File.open("api/v1/#{code}.geojson", 'w') {|w|
-      w.print JSON.dump({
-        :type => 'Feature',
-        :geometry => {
-          :type => 'Polygon',
-          :coordinates => [[
-            [lng0, lat0], [lng0, lat1], 
-            [lng1, lat1], [lng1, lat0], [lng0, lat0]
-          ]]
-        },
-        :properties => {
-          :code => code,
-          :url => "http://madefor.github.io/0410/api/v1/#{code}.geojson",
-          :view => "https://github.com/madefor/0410/blob/gh-pages/api/v1/#{code}.geojson"
-        }
-      })
+    dump("#{n1}#{n2}".to_i, n2 + 100.0, n1 * 2.0 / 3, (n2 + 1) + 100.0, (n1 + 1) * 2.0 / 3)
+    8.times{|n3|
+      8.times{|n4|
+        dump("#{n1}#{n2}#{n3}#{n4}".to_i, n2 + n4 / 8.0 + 100.0, (n1 + n3 / 8.0) * 2.0 / 3, (n2 + (n4 + 1) / 8.0) + 100.0, (n1 + (n3 + 1) / 8.0) * 2.0 / 3)
+      }
     }
   }
 }
-
