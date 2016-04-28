d9a374ec1c28d77fea4cc8c9934bbeb6a9860cfd
Hidenori FUJIMURA <hfu@si.local>
2016-04-28 22:39:25 +0900

    update

--- build.rb
+++ build.rb
@@ -2,10 +2,10 @@ require 'json'
 30.upto(68) {|n1|
   22.upto(53) {|n2|
     code = "#{n1}#{n2}".to_i
-    lng0 = n1 + 100.0
-    lat0 = n2 * 3.0 / 2
-    lng1 = (n1 + 1) + 100.0
-    lat1 = (n2 + 1) * 3.0 / 2
+    lng0 = n2 + 100.0
+    lat0 = n1 * 2.0 / 3
+    lng1 = (n2 + 1) + 100.0
+    lat1 = (n1 + 1) * 2.0 / 3
     File.open("api/v1/#{code}.geojson", 'w') {|w|
       w.print JSON.dump({
         :type => 'Feature',
