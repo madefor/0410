177dbfa8e169a583b7710041a451aad8dfa8116e
Hidenori FUJIMURA <hfu@si.local>
2016-04-28 22:33:15 +0900

    update

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 [wip] GeoJSON API for JIS X 0410
 
 ## Reference 
-JIS X 0410 地域メッシュコード http://kikakurui.com/x0/X0410-2002-01.html
+- JIS X 0410 地域メッシュコード http://kikakurui.com/x0/X0410-2002-01.html
+- http://www.stat.go.jp/data/mesh/pdf/gaiyo1.pdf
---
+++ build.rb
@@ -0,0 +1,27 @@
+require 'json'
+30.upto(68) {|n1|
+  22.upto(53) {|n2|
+    code = "#{n1}#{n2}".to_i
+    lng0 = n2 + 100.0
+    lat0 = n1 * 3.0 / 2
+    lng1 = (n2 + 1) + 100.0
+    lat1 = (n1 + 1) * 3.0 / 2
+    File.open("api/v1/#{code}.geojson", 'w') {|w|
+      w.print JSON.dump({
+        :type => 'Feature',
+        :geometry => {
+          :type => 'Polygon',
+          :coordinates => [[
+            [lng0, lat0], [lng0, lat1], 
+            [lng1, lat1], [lng1, lat0], [lng0, lat0]
+          ]]
+        },
+        :properties => {
+          :code => code,
+          :url => "http://madefor.github.io/0410/api/v1/#{code}.geojson"
+        }
+      })
+    }
+  }
+}
+
